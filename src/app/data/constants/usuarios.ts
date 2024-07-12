d24b11d8130387d6ac378e3b27cd0a332ee9a1d4
Marcos Vieira <marcosvieira482@gmail.com>
2024-07-12 19:29:21 -0300

    feat: update and make migrations

---
+++ src/app/data/constants/usuarios.ts
@@ -0,0 +1,24 @@
+import { Usuario } from "@/core/model/Usuario";
+
+const usuarios: Usuario[] = [
+    {
+        id: "1",
+        email: "maria@email.com",
+        nome: "Maria",
+        senha: "1234"
+    },
+
+    {
+        id: "2",
+        email: "jose@email.com",
+        nome: "Jose",
+        senha: "7894"
+    },
+
+    {
+        id: "2",
+        email: "carlos@email.com",
+        nome: "Carlos",
+        senha: "3214"
+    }
+]
