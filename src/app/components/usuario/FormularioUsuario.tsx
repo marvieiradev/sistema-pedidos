e4d8a429444119e61345c4ba6c9c9cd3bc8546a5
Marcos Vieira <marcosvieira482@gmail.com>
2024-07-22 19:18:01 -0300

    feat: fix FormularioUsuario erros

--- src/app/components/usuario/FormularioUsuario.tsx
+++ src/app/components/usuario/FormularioUsuario.tsx
@@ -11,16 +11,14 @@ export interface FormularioUsuarioProps {
 export default function FormularioUsuario(props: FormularioUsuarioProps) {
     return (
         <div className="flex flex-col gap-5">
-            <InputTexto label="Nome" type="text" value={props.usuario.nome} onChange={(e) => props.onChange?.({ ...props.usuario, nome: e.target.value })} />
-            <InputTexto label="Email" type="email" value={props.usuario.email} onChange={(e) => props.onChange?.({ ...props.usuario, email: e.target.value })} />
-            <InputTexto label="Senha" type="password" value={props.usuario.senha} onChange={(e) => props.onChange?.({ ...props.usuario, senha: e.target.value })} />
+            <InputTexto label="Nome" type="text" value={props.usuario.nome} onChange={(e) => props.onChange?.({ ...props.usuario, nome: e.currentTarget.value })} />
+            <InputTexto label="Email" type="email" value={props.usuario.email} onChange={(e) => props.onChange?.({ ...props.usuario, email: e.currentTarget.value })} />
+            <InputTexto label="Senha" type="password" value={props.usuario.senha} onChange={(e) => props.onChange?.({ ...props.usuario, senha: e.currentTarget.value })} />
 
             <div className="flex gap-5">
                 <button className="bg-green-800 text-white p-2 rounded-md" onClick={props.salvar}>Salvar</button>
                 <button className="bg-red-800 text-white p-2 rounded-md" onClick={props.cancelar}>Cancelar</button>
             </div>
         </div>
-
-
     )
 }
